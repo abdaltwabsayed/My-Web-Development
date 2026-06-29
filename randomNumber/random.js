5ce602d4efe80fdb47c05969443a6fdd3ab0ef02
Abdaltwab <abdaltwabsayed89@gmail.com>
2026-06-29 21:30:07 +0300

    Add result display and guess validation to random number generator

--- randomNumber/random.js
+++ randomNumber/random.js
@@ -4,15 +4,36 @@ const rollButton = document.getElementById("rollButton");
 const randomNumberLabel1 = document.getElementById("randomNumberLabel1");
 const randomNumberLabel2 = document.getElementById("randomNumberLabel2");
 const randomNumberLabel3 = document.getElementById("randomNumberLabel3");
+const result = document.getElementById("result");
 const min = 1;
 const max = 100;
-let randomNumber;
+let randomNumber1;
+let randomNumber2;
+let randomNumber3;
+let guess;
+let running = true;
+
+while(running){
+    guess = Number(window.prompt(`Guess a number between ${max} and ${min}.`));
+    if(!guess || guess === NaN || guess < min || guess > max){
+        continue;
+    }
+    running = false;
+}
 
 rollButton.onclick = function(){
-    randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    randomNumberLabel1.textContent = randomNumber;
-    randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    randomNumberLabel2.textContent = randomNumber;
-    randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    randomNumberLabel3.textContent = randomNumber;
+    randomNumber1 = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomNumberLabel1.textContent = randomNumber1;
+    randomNumber2 = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomNumberLabel2.textContent = randomNumber2;
+    randomNumber3 = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomNumberLabel3.textContent = randomNumber3;
+    if(guess == randomNumber1 || guess == randomNumber2 || guess == randomNumber3){
+        result.textContent = `Your guess ${guess} was correct!`;
+        result.style.backgroundColor = "green"; 
+    }
+    else {
+        result.textContent = `Your guess ${guess} was incorrect!`;
+        result.style.backgroundColor = "red"; 
+    }
 }
